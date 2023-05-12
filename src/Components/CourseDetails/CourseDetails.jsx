81aee0f4657585359662ca5e02d082f6eb15af09
MariaVan8 <fer_nandita_8@hotmail.com>
2023-05-12 12:52:41 -0700

    Updated Course Details with JSON file that wasmoved from server to client side

--- src/Components/CourseDetails/CourseDetails.jsx
+++ src/Components/CourseDetails/CourseDetails.jsx
@@ -1,36 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./CourseDetails.scss";
-import { useEffect, useState } from "react";
-import axios from "axios";
 import calendar from "../../assests/logo/calendar1.png";
 import laptop from "../../assests/logo/laptop1.png";
 import certificate from "../../assests/logo/certificate.png";
-
-const api = process.env.REACT_APP_API_URL;
+import coursesData from "../../data/courses.json";
 
 function CourseDetails() {
-  // const [courses, setCourses] = useState([]);
-
   const [course, setCourse] = useState({});
 
   const params = useParams();
 
   useEffect(() => {
-    axios
-      .get(`${api}/courses`)
-      .then((response) => {
-        console.log("test", response);
-        const foundCourse = response.data.find(
-          (course) => course.id === params.id
-        );
-        setCourse(foundCourse);
-        console.log(foundCourse);
-      })
-      .catch((err) => {
-        console.log("err: ", err);
-      });
-  }, []);
+    const foundCourse = coursesData.find((course) => course.id === params.id);
+    setCourse(foundCourse);
+  }, [params.id]);
 
   return (
     <>
@@ -44,15 +28,19 @@ function CourseDetails() {
           <h1 className="details__title">{course.title}</h1>
         </div>
         <div className="details__wrapper">
-          <img className="details__icon" src={calendar} />
+          <img className="details__icon" src={calendar} alt="calendar icon" />
           <p className="details__item">{course.time}</p>
         </div>
         <div className="details__wrapper">
-          <img className="details__icon" src={laptop} />
+          <img className="details__icon" src={laptop} alt="laptop icon" />
           <p className="details__item">{course.tools}</p>
         </div>
         <div className="details__wrapper">
-          <img className="details__icon" src={certificate} />
+          <img
+            className="details__icon"
+            src={certificate}
+            alt="certificate icon"
+          />
           <p className="details__item">Certificate</p>
         </div>
         <p className="details__desc">Description:</p>
